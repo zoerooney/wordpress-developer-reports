46dde3918bea789f5834a3c4dabd56f061395ce7
Zoe Rooney <zoe@zoerooney.com>
2013-11-30 07:24:18 -0500

    Printing some stuff
    Checking again what a few variables/ functions return
    
    Signed-off-by: Zoe Rooney <zoe@zoerooney.com>

--- wordpress-developer-reports.php
+++ wordpress-developer-reports.php
@@ -71,13 +71,15 @@ function wp_dev_reports_page_callback() {
 		<div class="module">
 			<h3>About WordPress</h3>
 			<p>You are running WordPress
-			<?php $version = get_bloginfo('version'); ?>
+			<?php print_r($wp_version); $version = get_bloginfo('version'); ?>
 			<strong><?php echo $version; ?></strong>.</p>
 			<?php $url = 'http://wordpress.org/latest';
 				  stream_context_set_default(
 				      array( 'http' => array( 'method' => 'HEAD' ) )
 				  );
 				  $headers = get_headers( $url ); ?>
+			<p>The latest version of WordPress is <?php print_r($headers); ?></p>
+			
 			<p>The latest version of WordPress is <?php echo $headers[8]; ?>. <?php if ( strpos( $headers[8], $version ) !== false ) { ?>You're up to date!<?php } else { ?>Please update!<?php } ?></p>
 		</div>
 		<form action="options.php" method="post">
