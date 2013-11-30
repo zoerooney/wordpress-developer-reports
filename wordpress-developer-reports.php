5c4daeb0e746f34654eeb5684b73d7589a61dac1
Zoe Rooney <zoe@zoerooney.com>
2013-11-30 00:38:36 -0500

    Testing version check
    Signed-off-by: Zoe Rooney <zoe@zoerooney.com>

--- wordpress-developer-reports.php
+++ wordpress-developer-reports.php
@@ -70,8 +70,8 @@ function wp_dev_reports_page_callback() {
 		</div>
 		<div class="module">
 			<h3>About WordPress</h3>
-			<p>You are running WordPress <?php echo $wp_version; ?> (global variable).</p>
-			<p>You are running WordPress <?php echo get_bloginfo('version'); ?> (bloginfo).</p>
+			<p>You are running WordPress <?php echo get_bloginfo('version'); ?>.</p>
+			<p>The latest version is <?php echo wp_version_check(); ?></p>
 		</div>
 		<form action="options.php" method="post">
 			<?php settings_fields('wp_dev_reports_options'); ?>
