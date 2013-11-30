d11f502e835f13d9be41af88c90ce824d0fc62cb
Zoe Rooney <zoe@zoerooney.com>
2013-11-29 23:26:31 -0500

    Still finding the error source
    Signed-off-by: Zoe Rooney <zoe@zoerooney.com>

--- wordpress-developer-reports.php
+++ wordpress-developer-reports.php
@@ -76,111 +76,113 @@ function wp_dev_reports_page_callback() {
 
 /* Register all the necessary settings
 =============================================*/
-
+/*
 add_action('admin_init', 'wp_dev_reports_admin_init');
 function wp_dev_reports_admin_init(){
 //  TO DO: NEED TO FIGURE OUT WHAT I WANT HERE, THIS IS ALL STUFF FROM JOYRIDE
-//
-//	register_setting(
-//		'wp_dev_reports_options',
-//		'wp_dev_reports_options',
-//		'wp_dev_reports_validation'
-//	);
-//	add_settings_section(
-//		'wp_dev_reports_display_options',
-//		'Display Options <i class="icon-cog"></i>',
-//		'wp_dev_reports_display_options_text',
-//		'wp_dev_reports'
-//	);
-//	add_settings_section(
-//		'wp_dev_reports_feature_tips',
-//		'Feature Tour Tips <i class="icon-map-marker"></i>',
-//		'wp_dev_reports_feature_tips_text',
-//		'wp_dev_reports'
-//	);
-//	
-//	// wp_dev_reports Options
-//	add_settings_field(
-//		'wp_dev_reports_tiplocation',
-//		'Tip location relative to the parent element',
-//		'wp_dev_reports_tiplocation_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_display_options'
-//	);
-//	add_settings_field(
-//		'wp_dev_reports_scrollspeed',
-//		'Page scroll speed, in ms',
-//		'wp_dev_reports_scrollspeed_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_display_options'
-//	);
-//	add_settings_field(
-//		'wp_dev_reports_timer',
-//		'Duration of each tour stop, 0 = off',
-//		'wp_dev_reports_timer_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_display_options'
-//	);
-//	add_settings_field(
-//		'wp_dev_reports_starttimeronclick',
-//		'Start the timer on the first click',
-//		'wp_dev_reports_starttimeronclick_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_display_options'
-//	);
-//	add_settings_field(
-//		'wp_dev_reports_nextbutton',
-//		'Show the "Next" button',
-//		'wp_dev_reports_nextbutton_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_display_options'
-//	);
-//	add_settings_field(
-//		'wp_dev_reports_tipanimation',
-//		'Animation style',
-//		'wp_dev_reports_tipanimation_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_display_options'
-//	);
-//	add_settings_field(
-//		'wp_dev_reports_tipanimationfadespeed',
-//		'Speed of the fade animation',
-//		'wp_dev_reports_tipanimationfadespeed_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_display_options'
-//	);
-//	
-//	// The tips
-//
-//	add_settings_field(
-//		'wp_dev_reports_tipid',
-//		'Tip ID (all lowercase, no spaces)',
-//		'wp_dev_reports_tipid_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_feature_tips'
-//	);
-//	add_settings_field(
-//		'wp_dev_reports_tipcontent',
-//		'Tip Content',
-//		'wp_dev_reports_tipcontent_input',
-//		'wp_dev_reports',
-//		'wp_dev_reports_feature_tips'
-//	);
+
+	register_setting(
+		'wp_dev_reports_options',
+		'wp_dev_reports_options',
+		'wp_dev_reports_validation'
+	);
+	add_settings_section(
+		'wp_dev_reports_display_options',
+		'Display Options <i class="icon-cog"></i>',
+		'wp_dev_reports_display_options_text',
+		'wp_dev_reports'
+	);
+	add_settings_section(
+		'wp_dev_reports_feature_tips',
+		'Feature Tour Tips <i class="icon-map-marker"></i>',
+		'wp_dev_reports_feature_tips_text',
+		'wp_dev_reports'
+	);
+	
+	// wp_dev_reports Options
+	add_settings_field(
+		'wp_dev_reports_tiplocation',
+		'Tip location relative to the parent element',
+		'wp_dev_reports_tiplocation_input',
+		'wp_dev_reports',
+		'wp_dev_reports_display_options'
+	);
+	add_settings_field(
+		'wp_dev_reports_scrollspeed',
+		'Page scroll speed, in ms',
+		'wp_dev_reports_scrollspeed_input',
+		'wp_dev_reports',
+		'wp_dev_reports_display_options'
+	);
+	add_settings_field(
+		'wp_dev_reports_timer',
+		'Duration of each tour stop, 0 = off',
+		'wp_dev_reports_timer_input',
+		'wp_dev_reports',
+		'wp_dev_reports_display_options'
+	);
+	add_settings_field(
+		'wp_dev_reports_starttimeronclick',
+		'Start the timer on the first click',
+		'wp_dev_reports_starttimeronclick_input',
+		'wp_dev_reports',
+		'wp_dev_reports_display_options'
+	);
+	add_settings_field(
+		'wp_dev_reports_nextbutton',
+		'Show the "Next" button',
+		'wp_dev_reports_nextbutton_input',
+		'wp_dev_reports',
+		'wp_dev_reports_display_options'
+	);
+	add_settings_field(
+		'wp_dev_reports_tipanimation',
+		'Animation style',
+		'wp_dev_reports_tipanimation_input',
+		'wp_dev_reports',
+		'wp_dev_reports_display_options'
+	);
+	add_settings_field(
+		'wp_dev_reports_tipanimationfadespeed',
+		'Speed of the fade animation',
+		'wp_dev_reports_tipanimationfadespeed_input',
+		'wp_dev_reports',
+		'wp_dev_reports_display_options'
+	);
+	
+	// The tips
+
+	add_settings_field(
+		'wp_dev_reports_tipid',
+		'Tip ID (all lowercase, no spaces)',
+		'wp_dev_reports_tipid_input',
+		'wp_dev_reports',
+		'wp_dev_reports_feature_tips'
+	);
+	add_settings_field(
+		'wp_dev_reports_tipcontent',
+		'Tip Content',
+		'wp_dev_reports_tipcontent_input',
+		'wp_dev_reports',
+		'wp_dev_reports_feature_tips'
+	);
 
 }
+*/
+
 
 /* Now, display all those settings
 =============================================*/
 //  TO DO: UPDATE FOR THIS PLUGIN
-//
+/*
 // Section header text
-//function wp_dev_reports_section_text() {
-//}
-//
+function wp_dev_reports_section_text() {
+}
+
 // Form fields
-//
-//function wp_dev_reports_tiplocation_input() {
-//	$options = get_option( 'wp_dev_reports_options' );?><!--
+
+function wp_dev_reports_tiplocation_input() {
+	$options = get_option( 'wp_dev_reports_options' );?>
 	
 	<select name="wp_dev_reports_options[tiplocation]" value="<?php echo $options[tiplocation]; ?>" />
 		<option value="top" <?php if ( $options['tiplocation'] == 'top' ) echo " selected='selected'";?>>Top</option>
@@ -215,47 +217,49 @@ function wp_dev_reports_tipanimation_input() {
 	<select name="wp_dev_reports_options[tipanimation]" value="<?php echo $options[tipanimation]; ?>" />
 		<option value="pop" <?php if ( $options['tipanimation'] == 'pop' ) echo " selected='selected'";?>>Pop</option>
 		<option value="fade" <?php if ( $options['tipanimation'] == 'fade' ) echo " selected='selected'";?>>Fade</option>
-	</select>-->
-<?php// }
-//
-//function wp_dev_reports_tipanimationfadespeed_input() {
-//	$options = get_option( 'wp_dev_reports_options' );
-//	echo '<input name="wp_dev_reports_options[tipanimationfadespeed]" type="text" size="5" value="' . $options[tipanimationfadespeed] . '" placeholder="300" /> ms';
-//}
-//
-//function wp_dev_reports_tipid_input() {
-//	$options = get_option( 'wp_dev_reports_options' );
-//	echo '<div><input name="wp_dev_reports_options[tipid]" type="text" value="' . $options[tipid] . '" />';
-//	echo '<input name="shortcode" type="text" value="[tourstop id=' . $options[tipid] . ']" readonly/>';
-//}
-//function wp_dev_reports_tipcontent_input() {
-//	echo '<textarea name="wp_dev_reports_options[tipcontent]">' . $options[tipcontent] . '</textarea></div>';
+	</select>
+<?php }
+
+function wp_dev_reports_tipanimationfadespeed_input() {
+	$options = get_option( 'wp_dev_reports_options' );
+	echo '<input name="wp_dev_reports_options[tipanimationfadespeed]" type="text" size="5" value="' . $options[tipanimationfadespeed] . '" placeholder="300" /> ms';
 }
 
-// Let's validate this stuff
+function wp_dev_reports_tipid_input() {
+	$options = get_option( 'wp_dev_reports_options' );
+	echo '<div><input name="wp_dev_reports_options[tipid]" type="text" value="' . $options[tipid] . '" />';
+	echo '<input name="shortcode" type="text" value="[tourstop id=' . $options[tipid] . ']" readonly/>';
+}
+function wp_dev_reports_tipcontent_input() {
+	echo '<textarea name="wp_dev_reports_options[tipcontent]">' . $options[tipcontent] . '</textarea></div>';
+}
+*/
+/**
+ * Let's validate this stuff
+ 
 function wp_dev_reports_validation( $input ) {
 //  TO DO: VALIDATION
-//
-//  if ( ! $input['scrollspeed'] )
-//    $input['scrollspeed'] = '300';
-//    
-//  if ( ! $input['timer'] )
-//    $input['timer'] = '2000';
-//  
-//  if ( ! $input['tipanimationfadespeed'] )
-//    $input['tipanimationfadespeed'] = '300';
-//      
-//  if ( ! isset( $input['starttimeronclick'] ) )
-//	$input['starttimeronclick'] = null;
-//	$input['starttimeronclick'] = ( $input['starttimeronclick'] == "true" ? "true" : "false" );
-//
-//  if ( ! isset( $input['nextbutton'] ) )
-//  	$input['nextbutton'] = null;
-//  	$input['nextbutton'] = ( $input['nextbutton'] == "true" ? "true" : "false" );
-//   	
-//  return $input;
-}
 
+  if ( ! $input['scrollspeed'] )
+    $input['scrollspeed'] = '300';
+    
+  if ( ! $input['timer'] )
+    $input['timer'] = '2000';
+  
+  if ( ! $input['tipanimationfadespeed'] )
+    $input['tipanimationfadespeed'] = '300';
+      
+  if ( ! isset( $input['starttimeronclick'] ) )
+	$input['starttimeronclick'] = null;
+	$input['starttimeronclick'] = ( $input['starttimeronclick'] == "true" ? "true" : "false" );
+
+  if ( ! isset( $input['nextbutton'] ) )
+  	$input['nextbutton'] = null;
+  	$input['nextbutton'] = ( $input['nextbutton'] == "true" ? "true" : "false" );
+   	
+  return $input;
+}
+*/ 
 // TO DO: NEED TO OUTPUT THIS STUFF SOMEPLACE...
 
 
